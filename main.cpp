ff800ccd49413cd68659941d29cc6edad0e13bff
Samuel Dominguez <sdomi003@ucr.edu>
2017-12-13 23:41:41 -0800

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <utility> 
+using namespace std;
+
+int main() {
+	//code
+	int num_tests = 0;
+    cin >> num_tests;
+	
+	for(int i = 0; i < num_tests; ++i ){        // do this num test times
+	    int N = 0;
+	    int X = 0;
+	    int Y = 0;
+	    vector <pair<int,int> > A;
+	    vector <pair<int,int> > B;
+	    
+	    
+	    cin >> N;
+	    cin >> X;
+	    cin >> Y;
+ 	    for (int j = 0; j < N; ++j){            // get input N times
+	        cout << "flag2" << endl;
+ 	    }
+	    
+	    
+	    
+	    
+	}
