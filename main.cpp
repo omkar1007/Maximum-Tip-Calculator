fffbde25580454ed66e4578932d227a9c9fdcbdc
Samuel Dominguez <sdomi003@ucr.edu>
2017-12-22 03:02:47 -0800

    final main.cpp
    finished

--- main.cpp
+++ main.cpp
@@ -1,55 +1,137 @@
+    /* Algorithm:
+    	     * create a class called "set" which holds 1 column of info (i.e. A's tip and B's tip), and also the difference A-B
+	     * create a vector that holds N sets
+ 	     * sort the vector from max to min according to the difference in each set, i.e. absolute value of A-B
+ 	     * vector is now max sorted according to the absolute value of (A-B)
+ 	     * while X and Y are both positive
+ 	     * 		if diff is pos, give tip to waiter A and --X
+ 	     * 		if diff is negative, give tip to B and --Y
+ 	     * if X is positive
+ 	     * 		give remaining tips to A
+ 	     * if Y is positive
+ 	     * 		give remaining tips to B
+ 	     */
+
+
+#include <iostream>
+#include <cmath>
 #include <iostream>
 #include <vector>
-#include <utility> 
+#include <utility>
 #include <algorithm>
 using namespace std;
+
+class set {
+public:
+	set(int a);
+
+	int a;
+	int b;
+	int diff;
+};
+
+set::set(int a){
+	this->a = a;
+	this->b = 0;
+	this->diff = 0;
+}
+
+struct myclass {
+  bool operator() (set i, set j) { return (abs(i.diff) > abs(j.diff));}
+} myobject;
+
 
 int main() {
 	//code
 	int num_tests = 0;
     cin >> num_tests;
-	
+    if(num_tests == 0){
+    		cout << 0 << endl;
+    		return 0;
+    }
+
 	for(int i = 0; i < num_tests; ++i ){        // do this num test times
 	    int N = 0;
 	    int X = 0;
 	    int Y = 0;
-	    vector <pair<int,int> > A;
-	    vector <pair<int,int> > B;
+	    vector <set> set_vector;
 	    int total_tips = 0;
-	    
 	    cin >> N;
 	    cin >> X;
 	    cin >> Y;
-	    
+
 	    int temp = 0;
- 	    for (int j = 0; j < N; ++j){            // get input N times
+ 	    for (int j = 0; j < N; ++j){            // get worker A input N times
  	        cin >> temp;
-	        A.push_back(make_pair (temp,j));
+ 	        set t(temp);
+ 	        set_vector.push_back(t);
  	    }
- 	    
- 	    for (int k = 0; k < N; ++k){            // get input N times
+
+ 	    for (int k = 0; k < N; ++k){            // get worker B input and fill out the abs difference N times
  	        cin >> temp;
-	        B.push_back(make_pair (temp,k));
+ 	        set_vector.at(k).b = temp;
+ 	        set_vector.at(k).diff = set_vector.at(k).a - temp;
  	    }
- 	    
-	    vector <pair<int,int> > sorted_A = A;
-	    vector <pair<int,int> > sorted_B = B;
-	    
-	    sort(sorted_A.begin(),sorted_A.end());
-	    sort(sorted_B.begin(),sorted_B.end());
-	    for(int r = 0; r < N; ++r){
-	       if((sorted_A.at(r).first > sorted_B.at(r).first) && X > 0){
-	           // issue, after the first check, I will need to be able to check
-	           // different indexes in the sorted arrays as I want to look for
-	           // the next highest availble. 
-	           // solution: delete whenever I use one, this way the highest
-	           // is always at the top of the vector.
-	       }
-	    }
-	    
-	    
+
+// 	    // TEST: print out the entire vector
+// 	    for(int z = 0; z < N; ++z){
+// 	    		cout << "A's tip: " << set_vector.at(z).a << endl;
+// 	    		cout << "B's tip: " << set_vector.at(z).b << endl;
+// 	    		cout << "abs_diff: " << set_vector.at(z).diff << endl;
+// 	    }
+ 	    // vector is filled
+
+ 	    //crappy selection sort
+
+// 	    for(int i = 0; i < N-1; ++i){
+// 	    		int max = i;
+// 	    		for(int j = i+1; j < N; ++j){
+// 	    			if( abs(set_vector.at(j).diff) > abs(set_vector.at(max).diff)){
+// 	    				max = j;
+// 	    			}
+// 	    		}
+// 	    		swap(set_vector.at(i), set_vector.at(max));
+// 	    }
+
+ 	    // std sort
+ 	   sort (set_vector.begin(), set_vector.end(), myobject);
+
+ 	   // 	    // TEST: is it in max according to abs diff
+//  	    	    for(int z = 0; z < N; ++z){
+//  	    	    		cout << "A's tip: " << set_vector.at(z).a << endl;
+//  	    	    		cout << "B's tip: " << set_vector.at(z).b << endl;
+//  	    	    		cout << "abs_diff: " << set_vector.at(z).diff << endl;
+//  	    	    }
+ 	    int curr = 0;
+ 	    while(X > 0 && Y > 0 && curr < N){
+ 	    		if(set_vector.at(curr).diff > 0){
+ 	    			total_tips += set_vector.at(curr).a;
+ 	    			--X;
+ 	    		 	cout << "given to X, total: " << total_tips << endl;
+ 	    		}
+ 	    		else{
+ 	    			total_tips += set_vector.at(curr).b;
+ 	    			--Y;
+ 	    		 	cout << "given to Y, total: " << total_tips << endl;
+ 	    		}
+ 	    		++curr;
+ 	    }
+//  	    cout << "curr: " << curr << endl;
+
+ 	   // now either X or Y or neither will be > 0
+ 	   while(X > 0 && curr < N){
+ 		  total_tips += set_vector.at(curr).a;
+ 		  --X;
+ 		  ++curr;
+ 	   }
+  	   while(Y > 0 && curr < N){
+  		  total_tips += set_vector.at(curr).b;
+  		  --Y;
+  		  ++curr;
+  	   }
+ 	   cout << total_tips << endl;
 	}
-	
-	
+
+
 	return 0;
 }
